78c09c957a96d7522ff41f72a9cde2e539e97b8c
Wesley van Tartwijk <w.vantartwijk@student.fontys.nl>
2024-06-05 16:28:36 +0200

    Installed Tailwind, added a responsive navbar.

--- portfoliosemester6/src/app/page.tsx
+++ portfoliosemester6/src/app/page.tsx
@@ -1,9 +1,15 @@
-import navBar from "./components/navbar"
-
 export default function Home() {
-    return(
-      <>
-      <h1>home 1</h1>
-      </>
-    )
+  return (
+    <>
+      <div className="container mx-auto px-4">
+        <h1>Hello World</h1>
+        <p>This is a sample page.</p>
+        <div className="w-full h-screen bg-green-600"></div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit
+        </p>
+      </div>
+
+    </>
+  );
 }
